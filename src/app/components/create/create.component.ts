f2e21edc5070fbfe1f3c41f15a10e338edbad8f3
Doryan Alexandro Mamani Ticona <doryanamt@gmail.com>
2024-11-11 23:20:15 +0100

    11/11/2024

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Departamento } from '../../models/departamento';
+// import { ServiceDepartamentos } from '../../services/service.departamento';
 
 @Component({
   selector: 'app-create',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+  @ViewChild("cajaid") cajaId!: ElementRef;
+  @ViewChild("cajanombre") cajaNombre!: ElementRef;
+  @ViewChild("cajalocalidad") cajaLocalidad!: ElementRef;
+  
+  public departamento !: Departamento;
 
+  constructor(){}
+  
+
+  nuevoDepartamento(): void{
+    let id = this.cajaId.nativeElement.value;
+    let nombre = this.cajaNombre.nativeElement.value;
+    let localidad = this.cajaLocalidad.nativeElement.value;
+    let departamentoNew = new Departamento(id, nombre, localidad);
+    console.log(departamentoNew);
+  }
 }
